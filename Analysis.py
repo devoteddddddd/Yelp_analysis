364f4621f5d7f42575c3164032492a9c47724708
devoteddddddd <99391825+devoteddddddd@users.noreply.github.com>
2023-06-06 10:37:45 +0800

    Update Analysis.py

--- Analysis.py
+++ Analysis.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="Analysis system for merchant")
 
 
 image1 = Image.open('./f1.png')
-
+image2 = Image.open('./f2.png')
 
 
 st.write("# Hi, Dear merchant👋")
@@ -21,3 +21,4 @@ st.markdown(
 )
 
 st.image(image1, caption='The top 15 commonly used attributes of Health and Medical businesses', use_column_width=True,  output_format='PNG')
+st.image(image2, caption='The top 50 commonly used categories of Health and Medical businesses', use_column_width=True,  output_format='PNG')
