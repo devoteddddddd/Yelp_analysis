eebcfa4b6981687fe1d1cb8994ab1222d2515a2c
devoteddddddd <99391825+devoteddddddd@users.noreply.github.com>
2023-06-06 20:49:30 +0800

    Update Analysis.py

--- Analysis.py
+++ Analysis.py
@@ -21,12 +21,9 @@ image10 = Image.open('./f10.png')
 image11 = Image.open('./f11.png')
 image12 = Image.open('./f12.png')
 
-df = pd.read_csv('./Health_business.csv')
-lats = df['latitude'].tolist()
-lons = df['longitude'].tolist()
-locations = list(zip(lats, lons))
-map1 = folium.Map(location=[37.0902, -95.7129], tiles='OpenStreetMap', zoom_start=5)
-FastMarkerCluster(data=locations).add_to(map1)
+
+
+
 
 st.write("# Hi, Dear merchant👋")
 
@@ -35,6 +32,16 @@ st.markdown(
     ### We provide our visualization results here for your reference! They are mainly about Health and Medical businesses in Yelp dataset.
     """
 )
+@st.cache(hash_funcs={"MyUnhashableClass": lambda _: None}, allow_output_mutation=True)
+def load():
+    df = pd.read_csv('./Health_business.csv')
+    lats = df['latitude'].tolist()
+    lons = df['longitude'].tolist()
+    locations = list(zip(lats, lons))
+    map1 = folium.Map(location=[37.0902, -95.7129], tiles='OpenStreetMap', zoom_start=5)
+    FastMarkerCluster(data=locations).add_to(map1)
+    return map1
+
 
 st.image(image1, caption='The top 15 commonly used attributes of Health and Medical businesses', use_column_width=True,  output_format='PNG')
 st.image(image2, caption='The top 50 commonly used categories of Health and Medical businesses', use_column_width=True,  output_format='PNG')
@@ -53,4 +60,5 @@ st.markdown(
     Here is the location distribution of all the Health and Medical businesses.
     """
 )
+map1 = load()
 st_data = st_folium(map1, width=725)
