efc70c31edf367dbae88d94d2e045afac458af87
devoteddddddd <99391825+devoteddddddd@users.noreply.github.com>
2023-06-06 14:57:56 +0800

    .

--- Analysis.py
+++ Analysis.py
@@ -10,6 +10,17 @@ st.set_page_config(page_title="Analysis system for merchant")
 
 image1 = Image.open('./f1.png')
 image2 = Image.open('./f2.png')
+image3 = Image.open('./f3.png')
+image4 = Image.open('./f4.png')
+image5 = Image.open('./f5.png')
+image6 = Image.open('./f6.png')
+image7 = Image.open('./f7.png')
+image8 = Image.open('./f8.png')
+image9 = Image.open('./f9.png')
+image10 = Image.open('./f10.png')
+image11 = Image.open('./f11.png')
+image12 = Image.open('./f12.png')
+
 df = pd.read_csv('./Health_business.csv')
 lats = df['latitude'].tolist()
 lons = df['longitude'].tolist()
@@ -21,10 +32,25 @@ st.write("# Hi, Dear merchant👋")
 
 st.markdown(
     """
-    We provide our visualization results here for your reference! They are mainly about Health and Medical businesses in Yelp dataset.
+    ### We provide our visualization results here for your reference! They are mainly about Health and Medical businesses in Yelp dataset.
     """
 )
 
 st.image(image1, caption='The top 15 commonly used attributes of Health and Medical businesses', use_column_width=True,  output_format='PNG')
 st.image(image2, caption='The top 50 commonly used categories of Health and Medical businesses', use_column_width=True,  output_format='PNG')
-st_data = st_folium(map1)
+st.image(image3, caption='The x-axis is the star rating, and the y-axis is the number of businesses possessing that rating', use_column_width=True,  output_format='PNG')
+st.image(image4, caption='Distribution of checkin count', use_column_width=True,  output_format='PNG')
+st.image(image5, caption='Distribution of review count', use_column_width=True,  output_format='PNG')
+st.image(image6, caption='Percentages of businesses at regional level', use_column_width=True,  output_format='PNG')
+st.image(image7, caption='Average rating of each category', use_column_width=True,  output_format='PNG')
+st.image(image8, caption='Features related to star rating', use_column_width=True,  output_format='PNG')
+st.image(image9, caption='Confusion matrix about features related to star rating', use_column_width=True,  output_format='PNG')
+st.image(image10, caption='Word cloud graph for all reviews of all Health and Medical businesses', use_column_width=True,  output_format='PNG')
+st.image(image11, caption='Word cloud graph for positive reviews of all Health and Medical businesses', use_column_width=True,  output_format='PNG')
+st.image(image12, caption='Word cloud graph for negative reviews of all Health and Medical businesses', use_column_width=True,  output_format='PNG')
+st.markdown(
+    """
+    Here is the location distribution of all the Health and Medical businesses.
+    """
+)
+st_data = st_folium(map1, width=725)
